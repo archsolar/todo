4bc93dc9de6154b197342742a11481e64d287a78
archsolar <m_charizard@hotmail.nl>
2023-11-21 04:22:41 +0100

    cleanup + moved code to other widgets.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,7 @@ import 'package:todo/constants.dart';
 import 'package:todo/database.dart';
 import 'package:todo/generic_widget.dart';
 
-// import 'task_list_screen.dart';
-
-class Global {
-  static final AppDatabase _database = AppDatabase();
-
-  static AppDatabase get database => _database;
-}
+import 'widgets.dart';
 
 void main() async {
   //obtain saved state.
@@ -175,60 +169,6 @@ class _MainScreenState extends State<MainScreen> {
               }
             },
           ),
-          // Row(
-          //   children: [
-          //     Flexible(
-          //         child: Container(
-          //       padding: const EdgeInsets.only(left: 10.0),
-          //       child:
-          //       // TextField(
-          //       //     maxLength: 255,
-          //       //     controller:
-          //       //         _textController, // Attach the TextEditingController
-          //       //     focusNode: _textFieldFocus, // Attach the FocusNode
-          //       //     canRequestFocus: true,
-          //       //     decoration: const InputDecoration.collapsed(
-          //       //       hintText: "New list",
-          //       //     ),
-          //       //     onSubmitted: (newListName) {
-          //   if (newListName.isEmpty) return;
-          //   try {
-          //     Global.database
-          //         .addList(TodoListsCompanion(
-          //             name: drift.Value(newListName),
-          //             profileId: drift.Value(_currentProfile!.id),
-          //             archived: drift.Value(false)))
-          //         .then((value) {
-          //       setState(() {});
-          //     });
-          //   } catch (e) {
-          //     // Show a Snackbar with an error message
-          //     final snackBar = SnackBar(
-          //         content: Text(
-          //             "An error occurred while adding the list."));
-          //     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          //   }
-          //   //TODO why does it keep focusing when I click on something else?
-          //   // Keep focus on the TextField
-          //   // Check if the text field has focus before refocusing
-          //   if (FocusScope.of(context).focusedChild ==
-          //       _textController) {
-          //     FocusScope.of(context).requestFocus(_textFieldFocus);
-          //   }
-          //   // Clear the TextField
-          //   _textController.clear();
-          //   FocusScope.of(context).requestFocus(new FocusNode());
-          // })
-          //           ,
-          //     )),
-          //     IconButton(
-          //       icon: const Icon(Icons.add),
-          //       onPressed: () {
-          //         // _addTask(_textController.text);
-          //       },
-          //     ),
-          //   ],
-          // ),
         )
       ],
     );
@@ -268,53 +208,6 @@ class _MainScreenState extends State<MainScreen> {
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       return null;
     }
-  }
-}
-
-class StreamDropDownButton extends StatelessWidget {
-  final Profile? currentProfile;
-  final Stream<List<Profile>> profileStream;
-  final Function(String) onChanged;
-
-  StreamDropDownButton({
-    required this.currentProfile,
-    required this.profileStream,
-    required this.onChanged,
-  });
-
-  /// Asynchronously builds a DropdownButton with profiles from the database.
-  /// Handles loading, errors, and allows the user to select a profile.
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: profileStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting ||
-            currentProfile == null) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return DropdownButton<String>(
-            value: currentProfile!.name,
-            onTap: () {
-              FocusScope.of(context).unfocus();
-            },
-            onChanged: (String? value) {
-              onChanged(value!);
-            },
-            items:
-                snapshot.data?.map<DropdownMenuItem<String>>((Profile value) {
-                      return DropdownMenuItem<String>(
-                        value: value.name,
-                        child: Text(value.name),
-                      );
-                    }).toList() ??
-                    [],
-          );
-        }
-      },
-    );
   }
 }
 
@@ -359,85 +252,5 @@ class TodoListPage extends StatelessWidget {
             ),
           ],
         ));
-  }
-}
-
-/// the TextField at the bottom of the page.
-class BottomInput extends StatefulWidget {
-  const BottomInput(
-      {super.key, required this.textSuggestion, required this.onSubmit});
-  final String textSuggestion;
-
-  ///
-  final Function(String) onSubmit;
-
-  @override
-  State<BottomInput> createState() => _BottomInputState();
-}
-
-class _BottomInputState extends State<BottomInput> {
-  final TextEditingController _textController = TextEditingController();
-  //TODO init and dispose
-  late FocusNode _textFieldFocus;
-
-  @override
-  void initState() {
-    super.initState();
-    _textFieldFocus = FocusNode();
-  }
-
-  @override
-  void dispose() {
-    _textFieldFocus.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-      ),
-      child: Row(
-        children: [
-          //TODO why flexible here?
-          Flexible(
-              child: Container(
-            padding: const EdgeInsets.only(left: 10.0),
-            //textfield
-            child: TextField(
-                maxLength: 255,
-                controller: _textController, // Attach the TextEditingController
-                focusNode: _textFieldFocus, // Attach the FocusNode
-                canRequestFocus: true,
-                decoration: const InputDecoration.collapsed(
-                  hintText: "Add to list",
-                ),
-                onSubmitted: (value) {
-                  onSubmit(value, context);
-                }),
-          )),
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              onSubmit(_textController.text, context);
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  void onSubmit(String value, BuildContext context) {
-    widget.onSubmit(value);
-    //TODO why does it keep focusing when I click on something else?
-    // Keep focus on the TextField
-    // Check if the text field has focus before refocusing
-    if (FocusScope.of(context).focusedChild == _textController) {
-      FocusScope.of(context).requestFocus(_textFieldFocus);
-    }
-    // Clear the TextField
-    _textController.clear();
-    FocusScope.of(context).requestFocus(new FocusNode());
   }
 }
